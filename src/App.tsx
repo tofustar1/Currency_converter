48d9870fb78d08026dcd3f764f469c9cf555efe0
Dmitrii Belovodskii <tofupages@gmail.com>
2024-06-27 07:30:58 +0600

    Реализовал конвертацию валют в файле App, с использованием useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { EUR_TO_USD, USD_TO_EUR } from "./constants";
 import './App.css';
 
-const App = () => (
-    <div className="container">
-      <div className="wrapper">
-        <div className="col">
-          <label htmlFor="USD">USD</label>
-          <input type="number" id="USD"/>
-        </div>
-        <div className="col">
-          <label htmlFor="EUR">EUR</label>
-          <input type="number" id="EUR"/>
+interface currenciesState {
+  usd: string;
+  eur: string;
+}
+
+const App = () => {
+  const [currencies, setCurrencies] = useState<currenciesState>({
+    usd: '',
+    eur: '',
+  });
+
+  const onCurrencyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target;
+    const parsedValue = parseFloat(value);
+
+    if (isNaN(parsedValue)) {
+      setCurrencies({
+        usd : '',
+        eur: ''
+      });
+      return;
+    }
+
+    if (name === 'usd') {
+      setCurrencies({
+        usd: value,
+        eur: (parsedValue * USD_TO_EUR).toString()
+      });
+    } else {
+      setCurrencies({
+        usd: (parsedValue * EUR_TO_USD).toString(),
+        eur: value
+      });
+    }
+  };
+
+  return (
+      <div className="container">
+        <div className="wrapper">
+          <div className="col">
+            <label htmlFor="usd">USD</label>
+            <input
+                onChange={onCurrencyChange}
+                type="number"
+                id="usd"
+                name="usd"
+                value={currencies.usd}
+            />
+          </div>
+          <div className="col">
+            <label htmlFor="EUR">EUR</label>
+            <input
+                onChange={onCurrencyChange}
+                type="number"
+                id="eur"
+                name="eur"
+                value={currencies.eur}
+            />
+          </div>
         </div>
       </div>
-    </div>
-);
+  )
+};
 
 export default App;
--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
-export const EURO_TO_DOLLAR = 1.07;
+export const EUR_TO_USD = 1.07;
+export const USD_TO_EUR = 0.94;
